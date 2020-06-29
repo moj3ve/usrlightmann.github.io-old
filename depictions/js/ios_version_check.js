802f0aabf8522ededd3dbcebcf5e53dc6bcd30e6
Lightmann <lightmanns11@gmail.com>
2020-06-28 20:19:03 -0400

    more changes

--- depictions/js/ios_version_check.js
+++ depictions/js/ios_version_check.js
@@ -46,10 +46,10 @@ Twitter - @TimonOlsthoorn
 
 
 // changed const to var for IE9/10 compatibity.
-var VERSION_CHECK_SUPPORTED = "Your iOS version is supported! ;";
-var VERSION_CHECK_NEEDS_UPGRADE = "Requires at least iOS %s &#x1f640;";
-var VERSION_CHECK_UNCONFIRMED = "Not yet tested on iOS %s &#x1f640;";
-var VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s &#x1f63f;";
+var VERSION_CHECK_SUPPORTED = "Your iOS version is supported!";
+var VERSION_CHECK_NEEDS_UPGRADE = "Requires at least iOS %s";
+var VERSION_CHECK_UNCONFIRMED = "Not yet tested on iOS %s";
+var VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s";
 
 function ios_version_check(minIOS,maxIOS,otherIOS,callBack) {
 	"use strict";
